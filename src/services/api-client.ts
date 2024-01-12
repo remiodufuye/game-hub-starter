3bb2ce725ada624b3b9aaa1166cc88a692b82df0
remiodufuye <remi.odufuye@gmail.com>
2024-01-11 20:38:36 -0500

    initial commit

---
+++ src/services/api-client.ts
@@ -0,0 +1,19 @@
+// import axios from "axios";
+
+// export default axios.create({
+//   baseURL: "https://api.rawg.io/api",
+//   params: {
+//     key: "c7b18323a47d40c394ea5b019646b1f5",
+//   },
+// });
+
+
+
+import axios from 'axios'
+
+export default axios.create({
+    baseURL:'https://api.rawg.io/api',
+    params:{
+        key:'023f17036ea2436b9dac06214f48ad66'
+    }
+})
