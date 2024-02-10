4e3f775293ec1c1953de01ee31078b0b9ce82a7a
Aderemi Odufuye <aderemiodufuye@macbook-air.myfiosgateway.com>
2024-02-09 21:03:07 -0500

    Refactoring-fetch games with React Query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,15 +1,10 @@
-// import axios from "axios";
-
-// export default axios.create({
-//   baseURL: "https://api.rawg.io/api",
-//   params: {
-//     key: "c7b18323a47d40c394ea5b019646b1f5",
-//   },
-// });
-
-
 
 import axios from 'axios'
+
+export interface FetchResponse<T> {
+    count: number;
+    results: T[];
+  }
 
 export default axios.create({
     baseURL:'https://api.rawg.io/api',
