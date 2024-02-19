4875b0de6e7e718b1b0e79c8742fc8980e1967f6
remiodufuye <remi.odufuye@gmail.com>
2024-02-19 10:44:30 -0500

    Implement Infinite Scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -30,8 +30,9 @@ useInfiniteQuery<FetchResponse<Game>,Error>({
 
         getNextPageParam: (lastPage,allPages) => {
            return  lastPage.next ? allPages.length + 1 : undefined;
-        }
-
+        },
+    
+        staleTime: 24 * 60 * 60 * 1000
 
   })
 
