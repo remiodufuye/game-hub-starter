e165ca3781a4980b8b0bad7bd23a67a7016358c8
remiodufuye <remi.odufuye@gmail.com>
2024-11-27 17:52:54 -0500

    Handle Errors

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,23 @@
+import { Heading, Text, Box } from "@chakra-ui/react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  return (
+    <>
+      <NavBar />
+      <Box padding={5}>
+        <Heading>Oops</Heading>
+        <Text>
+          {isRouteErrorResponse(error)
+            ? "This Page Does not exist ."
+            : "An unexpected error occured."}
+        </Text>
+      </Box>
+    </>
+  );
+};
+
+export default ErrorPage;
