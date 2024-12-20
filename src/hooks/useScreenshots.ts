0764abae3b0bf1d504bd350756b5d48ce6a589df
remiodufuye <remi.odufuye@gmail.com>
2024-12-20 16:47:17 -0500

    Build Game Screen Shot

---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { Screenshot } from "../entities/Screenshot";
+import ApiClient from "../services/api-client";
+
+
+const useScreenshots = (gameId:number) => {
+    const apiClient = new ApiClient<Screenshot>(`/games/${gameId}/screenshots`)
+    return useQuery({
+        queryKey:['screenshots',gameId],
+        queryFn: apiClient.getAll
+    })
+} 
+
+export default useScreenshots
